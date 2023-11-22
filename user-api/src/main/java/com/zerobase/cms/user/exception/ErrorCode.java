b6fbe91cf43cbdfc90429bed7e23b42900eb219f
Taejun Park <ekswn120@gmail.com>
2023-11-22 17:40:19 +0900

    feat : [003-EMAIL-VERIFICATION] 커스터머 이메일 verification

---
+++ user-api/src/main/java/com/zerobase/cms/user/exception/ErrorCode.java
@@ -0,0 +1,21 @@
+package com.zerobase.cms.user.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+    ALREADY_REGISTERED_USER(BAD_REQUEST, "이미 가입된 회원입니다."),
+    WRONG_VERIFICATION(BAD_REQUEST, "잘못된 인증 시도입니다."),
+    EXPIRE_CODE(BAD_REQUEST, "인증 시간이 만료되었습니다."),
+    NOT_FOUND_USER(BAD_REQUEST, "일치하는 회원이 없습니다."),
+    ALREADY_VERIFIED(BAD_REQUEST, "이미 인증이 완료되었습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String detail;
+}
--- user-api/src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
+++ user-api/src/main/java/com/zerobase/cms/user/service/SignUpCustomerService.java
@@ -3,8 +3,16 @@ package com.zerobase.cms.user.service;
 import com.zerobase.cms.user.domain.SignUpForm;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.domain.repository.CustomerRepository;
+import com.zerobase.cms.user.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
+
+import static com.zerobase.cms.user.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +22,45 @@ public class SignUpCustomerService {
 
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
+    }
+
+    public boolean isEmailExist(String email) {
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT))
+                .isPresent();
+    }
+
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(() -> new CustomException(NOT_FOUND_USER));
+
+        if (customer.isVerify()) {
+            throw new CustomException(ALREADY_VERIFIED);
+        }
+        if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomException(WRONG_VERIFICATION);
+        }
+        if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(EXPIRE_CODE);
+        }
+        customer.setVerify(true);
+    }
+
+    @Transactional
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional =
+                customerRepository.findById(customerId);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+
+            customer.setVerificationCode(verificationCode);
+            customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1));
+
+            return customer.getVerifyExpiredAt();
+        } else {
+            throw new CustomException(NOT_FOUND_USER);
+        }
+
     }
 }
